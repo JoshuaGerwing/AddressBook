bd5cb9bb3e391f30c0673d8bc36de5db61ae648f
JoshuaGerwing <josh.gerwing@gmail.com>
2020-02-05 13:30:52 -0500

    add travis and tests

--- src/test/java/gerwingsoft/AddressBook/AddressBookApplicationTests.java
+++ src/test/java/gerwingsoft/AddressBook/AddressBookApplicationTests.java
@@ -1,13 +1,27 @@
 package gerwingsoft.AddressBook;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-class AddressBookApplicationTests {
+@AutoConfigureMockMvc
+public class AddressBookApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void createAddress() throws Exception {
+		this.mockMvc.perform(get("/createAddressBook")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("{\"id\":4,\"buddyInfoList\":[]}")));
 	}
-
 }
